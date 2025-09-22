fa48d9984c10bb61df0e3c7c86b98c68c7998b45
klterwelp <111607397+klterwelp@users.noreply.github.com>
2025-09-22 13:51:22 -0600

    Add environment to documentation

--- scripts/02_update_taxonomy.sh
+++ scripts/02_update_taxonomy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script updates the taxonomy tables from 01 to 2025-06-01 NCBI taxonomy. 
+# This script updates the taxonomy tables from 01 to 2025-06-01 NCBI taxonomy.
 # Inputs:
 # intermediates/TSV/
 #   - mgx_last_tax.tsv: last identified taxa and its rank per ASV for mgx
@@ -8,18 +8,21 @@
 # references/NCBI_blast/
 #   - names.dmp: NCBI taxonomy names file
 #   - nodes.dmp: NCBI taxonomy nodes file
-#   - from the NCBI taxonomy archive for taxdmp_2025-06-01.zip 
-# Outputs: 
+#   - from the NCBI taxonomy archive for taxdmp_2025-06-01.zip
+# Environment:
+#  - taxonkit conda environment from environments/taxonkit.yml
+#  - requires wget installed
+# Outputs:
 # intermediates/TSV/
 #   - up_fl_tax.tsv: updated taxonomy for FL-16S
 #   - up_v1v3_tax.tsv: updated taxonomy for 16S V1-V3
 #   - up_mgx_tax.tsv: updated taxonomy for MGX
-#   - Format of TSV Output Files: 
-#       old_name (from 01), ncbi_taxid, ncbi taxonomic level, 
+#   - Format of TSV Output Files:
+#       old_name (from 01), ncbi_taxid, ncbi taxonomic level,
 #       full lineage, scientific NCBI name, short lineage (Kingdom;Phylum;Class;Order;Family;Genus;Species)
 
-# Download the latest NCBI taxonomy files 
-NCBI_FOL="../references/NCBI_blast" 
+# Download the latest NCBI taxonomy files
+NCBI_FOL="../references/NCBI_blast"
 # if names.dmp already exist, skip download
 if [ -f "${NCBI_FOL}/names.dmp" ]; then
     echo "NCBI taxonomy files already exist in $NCBI_FOL. Skipping download."
@@ -35,14 +38,14 @@ INT_TSV="../intermediates/TSV"
 FL_TAX="${INT_TSV}/fl_last_tax.tsv"
 V1V3_TAX="${INT_TSV}/v1v3_last_tax.tsv"
 MGX_TAX="${INT_TSV}/mgx_last_tax.tsv"
-# Path to output files 
+# Path to output files
 FL_OUT="${INT_TSV}/up_fl_tax.tsv"
 V1V3_OUT="${INT_TSV}/up_v1v3_tax.tsv"
 MGX_OUT="${INT_TSV}/up_mgx_tax.tsv"
 
 # Activate the conda environment
 # Initialize conda for non-interactive shells and activate taxonkit environment
-source activate taxonkit
+conda activate taxonkit
 # set TAXONKIT_DB to the path to the NCBI taxonomy files
 export TAXONKIT_DB="$NCBI_FOL"
 taxonkit version
